8023e97311fc83e4c647d0ece29273d5ea40ad0d
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2019-06-16 21:59:10 -0500

    Base project

---
+++ src/main/java/com/grayraccoon/postgresjsonb/PostgresjsonbApplication.java
@@ -0,0 +1,13 @@
+package com.grayraccoon.postgresjsonb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresjsonbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresjsonbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grayraccoon/postgresjsonb/PostgresjsonbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.grayraccoon.postgresjsonb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostgresjsonbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
